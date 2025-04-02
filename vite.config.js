460b796eea24def9f5957102f7b024f4f01a92ea
PARMANAND RAY <156565937+parmanand-ray@users.noreply.github.com>
2025-04-02 14:03:58 +0530

    Update vite.config.js
    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000, // Use Render's port or default to 3000
-    strictPort: true, // Ensures Vite runs on the specified port
-    host: '0.0.0.0', // Required for Render deployment
-  },
 });
